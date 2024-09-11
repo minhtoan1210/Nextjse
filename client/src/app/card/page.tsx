be63841c089a3683c5ef4258e6d34a459d89b2c6
T Pin <ptpin7672@gmail.com>
2024-09-11 17:21:27 +0700

    css module và css thường

---
+++ client/src/app/card/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/style.css";
+import style from "./css/style.module.css";
+
+export default function Card() {
+  return (
+    <div>
+      <h1>Minh</h1>
+      <h1 className={style.h2}>Toàn</h1>
+    </div>
+  );
+}
