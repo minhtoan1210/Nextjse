13db5756f290e7e09a8af32a866848d4cffcee59
T Pin <ptpin7672@gmail.com>
2024-09-16 16:34:42 +0700

    add-font-images

--- client/src/app/card/page.tsx
+++ client/src/app/card/page.tsx
@@ -5,7 +5,7 @@ import style from "./css/style.module.css";
 export default function Card() {
   return (
     <div>
-      <h1>Minh</h1>
+      <h1 className="font-thin">Minh</h1>
       <h1 className={style.h2}>Toàn</h1>
     </div>
   );
